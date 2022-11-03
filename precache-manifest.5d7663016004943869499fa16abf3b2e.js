1d5a92275dc1c644654ac4eee6a78ee95cbc9499
Chandan <chandan.nayak0297@gmail.com>
2022-11-03 10:39:05 +0530

    Updates

---
+++ precache-manifest.5d7663016004943869499fa16abf3b2e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "585d0dec9a8b77b9883a",
+    "url": "/textutils/static/css/main.22765a8e.chunk.css"
+  },
+  {
+    "revision": "585d0dec9a8b77b9883a",
+    "url": "/textutils/static/js/main.585d0dec.chunk.js"
+  },
+  {
+    "revision": "bfaef02fc73415496900",
+    "url": "/textutils/static/js/1.bfaef02f.chunk.js"
+  },
+  {
+    "revision": "31d77044af729c125605",
+    "url": "/textutils/static/js/2.31d77044.chunk.js"
+  },
+  {
+    "revision": "ff0434560e28d9826c57",
+    "url": "/textutils/static/js/runtime~main.ff043456.js"
+  },
+  {
+    "revision": "ba703bdb34e6e08c48e7e4276f85748c",
+    "url": "/textutils/index.html"
+  }
+];
